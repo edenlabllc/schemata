548d5accec1e4505e0cffc1f34ec55ece2343659
dzakurakin <dzakurakin@edenlab.com.ua>
2021-04-08 23:10:13 +0300

    feat: allow null value in enum

--- lib/definitions/enum.ex
+++ lib/definitions/enum.ex
@@ -7,13 +7,19 @@ defmodule Schemata.Definitions.Enum do
 
   defstruct type: "string",
             enum: nil,
-            opts: [callbacks: []]
+            opts: [
+              callbacks: [],
+              null: false
+            ]
 
   def enum(values) do
     %__MODULE__{enum: values}
   end
 
   def enum(values, opts) do
-    %__MODULE__{enum: values, opts: opts}
+    nullable? = Keyword.get(opts, :null)
+    type = if nullable?, do: ["string", "null"], else: "string"
+    values = if nullable?, do: values ++ [nil], else: values
+    %__MODULE__{enum: values, opts: opts, type: type}
   end
 end
