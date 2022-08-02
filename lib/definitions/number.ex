04dfbd803151e9d179fb2208ff27382523018462
dzakurakin <dzakurakin@edenlab.com.ua>
2022-08-02 19:36:56 +0300

    feat: add number multipleOf property

--- lib/definitions/number.ex
+++ lib/definitions/number.ex
@@ -19,6 +19,7 @@ defimpl Jason.Encoder, for: Schemata.Definitions.Number do
     encode_value =
       %{}
       |> add_type(value, "number")
+      |> add_not_null_value(value, :multipleOf)
       |> add_not_null_value(value, :minimum)
       |> add_not_null_value(value, :exclusiveMinimum)
       |> add_not_null_value(value, :maximum)
